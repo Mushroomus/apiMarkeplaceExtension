ef4caeb0100aab7aafb11c172d07cd240ada2dec
Piotr <Piotr@DESKTOP-OGB5VIU>
2022-10-30 22:32:17 +0100

    add + remove works

--- contentScript.js
+++ contentScript.js
@@ -1,39 +1,85 @@
 const url = "http://localhost:3000/item";
-const options = {
-  method: "POST",
-  mode: "no-cors",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json;charset=UTF-8",
-  }, body: JSON.stringify({
-    "sku": "534;9",
-    "name": "itemTest",
-    "price": 12.34,
-    "isCraftable": "1",
-    "quality": "Unique"
-  })
-};
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if( request.message === "addItem" ) {
+      console.log("test");
+      var tabUrl = request.tabUrl;
+      var isCraftable = "1";
+      var sku = tabUrl.replace('https://marketplace.tf/items/tf2/', '');
+      var type = "ext";
+      var quality = "Unique";
+      var price = parseFloat( document.body.getElementsByClassName("current-bid-amount")[0].innerHTML.replace(' each', '').replace('$','') );
+
+      var name = document.body.getElementsByClassName("item-title")[0].getElementsByTagName("span")[0].innerHTML;
+      
+      if(name.includes("Taunt:"))
+      {
+        name = name.replace("Taunt: ", "");
+        type = "Taunt";
+      }
+
+      if(name.includes("Strange Part:"))
+      {
+        name = name.replace("Strange Part: ", "");
+        type = "Strange Part";
+      }
+
+      if(name.includes("Strange"))
+      {
+        if(name.includes("Vintage"))
+        {
+          name = name.replace("Vintage Strange ", "");
+          quality = "Vintage Strange";
+        }
+        else if(name.includes("Haunted"))
+        {
+          name = name.replace("Haunted Strange " , "");
+          quality = "Haunted Strange";
+        }
+        else if(name.includes("Genuine"))
+        {
+          name = name.replace("Genuine Strange " , "");
+          quality = "Genuine Strange";
+        }
+        else
+        {
+          name = name.replace("Strange ", "" );
+          quality = "Strange";
+        }
+      }
+      else if(name.includes("Vintage"))
+      {
+        name = name.replace("Vintage ", "");
+        quality = "Vintage";
+      }
+      else if(name.includes("Haunted"))
+      {
+        name = name.replace("Haunted " , "");
+        quality = "Haunted";
+      }
+      else if(name.includes("Genuine"))
+      {
+        name = name.replace("Genuine " , "");
+        quality = "Genuine";
+      }
+
+
+      if(tabUrl.includes("uncraftable"))
+        isCraftable = "0";
+
         fetch(url, {
             method: "POST",
-            mode: "no-cors",
             headers: {
               Accept: "application/json",
               "Content-Type": "application/json;charset=UTF-8",
             }, body: JSON.stringify({
-              "sku": "sda",
-              "name": "itemTest",
-              "price": parseFloat( document.body.getElementsByClassName("current-bid-amount")[0].innerHTML.replace(' each', '').replace('$','') ),
-              "isCraftable": "1",
-              "quality": "Unique"
+              "sku": sku,
+              "name": name,
+              "price": price,
+              "isCraftable": isCraftable,
+              "type": type,
+              "quality": quality
             })
         })
-        .then(response => {
-            return response.text()
-            })
-        }
     }
   );
--- popup.js
+++ popup.js
@@ -1,22 +1,44 @@
 
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    
     var url = tabs[0].url;
+    var apiSku = "http://localhost:3000/item/" + url.replace('https://marketplace.tf/items/tf2/', '');
     
     if( url.includes("https://marketplace.tf/items/tf2/"))
     {
-        var buttonAdd = document.createElement('button');
-        buttonAdd.innerHTML = 'Add Item to Database';
-
-
-        document.getElementsByClassName("btn")[1].style.visibility = "hidden";
-        document.getElementsByClassName("btn")[2].style.visibility = "hidden";
-
-        document.getElementsByClassName("btn")[0].onclick = function () { 
-            chrome.tabs.sendMessage(tabs[0].id, {"message": "addItem"}); 
-        };
-
+        fetch(apiSku, {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json;charset=UTF-8",
+            }
+        })
+        .then( (response) => response.json())
+        .then( (data) => {
+            if(data.message == "item does not exist")
+            {
+                document.getElementsByClassName("btn")[1].style.visibility = "hidden";
+                document.getElementsByClassName("btn")[2].style.visibility = "hidden";
         
-        //document.getElementById("buttons")appendChild(buttonAdd);
+                document.getElementsByClassName("btn")[0].onclick = function () { 
+                    chrome.tabs.sendMessage(tabs[0].id, {"message": "addItem", "tabUrl": url}); 
+                };
+            }
+            else 
+            {
+                document.getElementsByClassName("btn")[0].style.visibility = "hidden";
+
+                document.getElementsByClassName("btn")[2].onclick = function() {
+                    fetch(apiSku, {
+                        method: "DELETE",
+                        headers: {
+                          Accept: "application/json",
+                          "Content-Type": "application/json;charset=UTF-8",
+                        }
+                    })
+                }
+            }
+        })
     }
     else
         document.getElementById("buttons").innerHTML = "Need to be on Marketplace.tf";
